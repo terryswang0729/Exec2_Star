6ab474f734c161d1b9f4ed0a72f9a540fabc896c
ispan <ispan@DESKTOP-PAKSETB>
2022-10-26 09:43:20 +0800

    加入專案檔案。

---
+++ Exec2_Star/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Exec2_Star
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.leftStarsButton = new System.Windows.Forms.Button();
+			this.rowsLabel = new System.Windows.Forms.Label();
+			this.rowsTextBox = new System.Windows.Forms.TextBox();
+			this.midStarsButton = new System.Windows.Forms.Button();
+			this.RightStarsButton = new System.Windows.Forms.Button();
+			this.resultTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// leftStarsButton
+			// 
+			this.leftStarsButton.Font = new System.Drawing.Font("微軟正黑體", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.leftStarsButton.Location = new System.Drawing.Point(280, 57);
+			this.leftStarsButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.leftStarsButton.Name = "leftStarsButton";
+			this.leftStarsButton.Size = new System.Drawing.Size(70, 33);
+			this.leftStarsButton.TabIndex = 0;
+			this.leftStarsButton.Text = "靠左星號";
+			this.leftStarsButton.UseVisualStyleBackColor = true;
+			this.leftStarsButton.Click += new System.EventHandler(this.leftStarsButton_Click);
+			// 
+			// rowsLabel
+			// 
+			this.rowsLabel.AutoSize = true;
+			this.rowsLabel.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.rowsLabel.Location = new System.Drawing.Point(55, 65);
+			this.rowsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.rowsLabel.Name = "rowsLabel";
+			this.rowsLabel.Size = new System.Drawing.Size(41, 20);
+			this.rowsLabel.TabIndex = 1;
+			this.rowsLabel.Text = "列數";
+			// 
+			// rowsTextBox
+			// 
+			this.rowsTextBox.Location = new System.Drawing.Point(98, 65);
+			this.rowsTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.rowsTextBox.Multiline = true;
+			this.rowsTextBox.Name = "rowsTextBox";
+			this.rowsTextBox.Size = new System.Drawing.Size(134, 21);
+			this.rowsTextBox.TabIndex = 2;
+			// 
+			// midStarsButton
+			// 
+			this.midStarsButton.Font = new System.Drawing.Font("微軟正黑體", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.midStarsButton.Location = new System.Drawing.Point(355, 57);
+			this.midStarsButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.midStarsButton.Name = "midStarsButton";
+			this.midStarsButton.Size = new System.Drawing.Size(70, 33);
+			this.midStarsButton.TabIndex = 3;
+			this.midStarsButton.Text = "等腰星號";
+			this.midStarsButton.UseVisualStyleBackColor = true;
+			this.midStarsButton.Click += new System.EventHandler(this.midStarsButton_Click);
+			// 
+			// RightStarsButton
+			// 
+			this.RightStarsButton.Font = new System.Drawing.Font("微軟正黑體", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.RightStarsButton.Location = new System.Drawing.Point(430, 57);
+			this.RightStarsButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.RightStarsButton.Name = "RightStarsButton";
+			this.RightStarsButton.Size = new System.Drawing.Size(70, 33);
+			this.RightStarsButton.TabIndex = 4;
+			this.RightStarsButton.Text = "靠右星號";
+			this.RightStarsButton.UseVisualStyleBackColor = true;
+			this.RightStarsButton.Click += new System.EventHandler(this.RightStarsButton_Click);
+			// 
+			// resultTextBox
+			// 
+			this.resultTextBox.Location = new System.Drawing.Point(58, 114);
+			this.resultTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.resultTextBox.Multiline = true;
+			this.resultTextBox.Name = "resultTextBox";
+			this.resultTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.resultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.resultTextBox.Size = new System.Drawing.Size(448, 192);
+			this.resultTextBox.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(600, 360);
+			this.Controls.Add(this.resultTextBox);
+			this.Controls.Add(this.RightStarsButton);
+			this.Controls.Add(this.midStarsButton);
+			this.Controls.Add(this.rowsTextBox);
+			this.Controls.Add(this.rowsLabel);
+			this.Controls.Add(this.leftStarsButton);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button leftStarsButton;
+		private System.Windows.Forms.Label rowsLabel;
+		private System.Windows.Forms.TextBox rowsTextBox;
+		private System.Windows.Forms.Button midStarsButton;
+		private System.Windows.Forms.Button RightStarsButton;
+		private System.Windows.Forms.TextBox resultTextBox;
+	}
+}
+
---
+++ Exec2_Star/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_Star
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void leftStarsButton_Click(object sender, EventArgs e)
+		{
+			int? rows = GetRows();
+			if (rows.HasValue == false)
+			{
+				MessageBox.Show("請輸入列數");
+				return;
+			}
+			if (rows.Value < 0)
+			{
+				MessageBox.Show("列數必須大於零");
+				return;
+			}
+			string stars = GenerateLeftStars(rows.Value);
+
+			resultTextBox.Text = stars;
+		}
+
+		private void midStarsButton_Click(object sender, EventArgs e)
+		{
+			int? rows = GetRows();
+			if (rows.HasValue == false)
+			{
+				MessageBox.Show("請輸入列數");
+				return;
+			}
+			if (rows.Value < 0)
+			{
+				MessageBox.Show("列數必須大於零");
+				return;
+			}
+			string stars = GenerateMidStars(rows.Value);
+
+			resultTextBox.Text = stars;
+
+		}
+
+		private void RightStarsButton_Click(object sender, EventArgs e)
+		{
+			int? rows = GetRows();
+			if (rows.HasValue == false)
+			{
+				MessageBox.Show("請輸入列數");
+				return;
+			}
+			if (rows.Value < 0)
+			{
+				MessageBox.Show("列數必須大於零");
+				return;
+			}
+			string stars = GenerateRightStars(rows.Value);
+
+			resultTextBox.Text = stars;
+
+		}
+		private int? GetRows()
+		{
+			string input = rowsTextBox.Text;
+			bool isInt = int.TryParse(input, out int rows);
+			if (isInt)
+			{
+				return rows;
+			}
+			else
+			{
+				return null;
+			}
+		}
+		private string GenerateLeftStars(int rows)
+		{
+			string result = string.Empty;
+			for (int i = 1; i <= rows; i++)
+			{
+				result += new string('*', i) + "\r\n";
+			}
+			return result;
+		}
+		private string GenerateMidStars(int rows)
+		{
+			string result = string.Empty;
+
+			for (int i = 1; i <= rows; i++)
+			{
+				result += new string(' ', (rows - i)*2) + new string('*', 2 * i - 1)+"\r\n";
+				
+			}
+			return result;
+		}
+		private string GenerateRightStars(int rows)
+		{
+			string result = string.Empty;
+			
+			for (int i = 1; i<=rows ; i++)
+			{
+				result += new string(' ', (rows-i)*2) + new string('*',i) + "\r\n";
+
+			}
+			return result;
+		}
+	}
+}
